6d43e7f98fee39d13b61a5061dd63c43e959e663
HannaVelychko <anna.ua19@gmail.com>
2022-10-04 23:31:52 +0200

    Added Search engine

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,15 +1,24 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        <a href="#" className="btn btn-primary shadow">
-          Search
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by Hanna Velychko and is open-sourced on
+            GitHub and hosted on Netlify
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
